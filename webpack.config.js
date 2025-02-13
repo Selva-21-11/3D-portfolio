ebe0dfa27a6b03f37a52095aa4502f34eb869b9f
selvaranjan <selvacricketer21@gmail.com>
2025-02-14 00:08:03 +0530

    test3

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: "/3D-portfolio/",
+        publicPath: process.env.NODE_ENV === 'production' ? "/3D-portfolio/" : "/",
     },
     module: {
         rules: [
